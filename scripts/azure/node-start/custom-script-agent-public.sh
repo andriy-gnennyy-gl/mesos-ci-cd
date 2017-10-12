4dddb4b4b50fc8674cd78629f04634cd2585a5b1
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2017-10-12 15:48:31 +0300

    No permissions for CIFS share mounts

--- scripts/azure/node-start/custom-script-agent-public.sh
+++ scripts/azure/node-start/custom-script-agent-public.sh
@@ -3,7 +3,7 @@
 date
 
 echo "-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/"
-echo "Applying customizations for DC/OS Public Agent node..."
+echo "v1.1: Applying customizations for DC/OS Public Agent node..."
 echo "-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/"
 
 mkdir -p /dcos/volume0
@@ -17,15 +17,15 @@ mkdir -p /dcos/volume7
 mkdir -p /dcos/volume8
 mkdir -p /dcos/volume9
 
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume0 /dcos/volume0 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume1 /dcos/volume1 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume2 /dcos/volume2 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume3 /dcos/volume3 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume4 /dcos/volume4 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume5 /dcos/volume5 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume6 /dcos/volume6 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume7 /dcos/volume7 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume8 /dcos/volume8 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
-mount -t cifs //mesosvolumes01.file.core.windows.net/volume9 /dcos/volume9 -o vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume0 /dcos/volume0 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume1 /dcos/volume1 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume2 /dcos/volume2 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume3 /dcos/volume3 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume4 /dcos/volume4 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume5 /dcos/volume5 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume6 /dcos/volume6 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume7 /dcos/volume7 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume8 /dcos/volume8 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
+mount -t cifs //mesosvolumes01.file.core.windows.net/volume9 /dcos/volume9 -o noperm,vers=3.0,username=mesosvolumes01,password="VPqUJn5SWBzEs5crm8iaXKvMvjp7OVROZOuKdyMM+Qu7SiwcK3Xj5gfl/67h2NILFhxP/yte4ZBxife5OvTMxw==",dir_mode=0777,file_mode=0777
 
 
