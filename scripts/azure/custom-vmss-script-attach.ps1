717e11e85ea6af918a4b13c9a75968611292ab87
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2017-10-12 12:31:26 +0300

    More scripts

--- scripts/azure/custom-vmss-script-attach.ps1
+++ scripts/azure/custom-vmss-script-attach.ps1
@@ -1,3 +1,4 @@
 
+# Ideas from https://github.com/MicrosoftDocs/azure-docs/blob/master/articles/virtual-machine-scale-sets/virtual-machine-scale-sets-deploy-app.md
 
 az vmss extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --resource-group mesospoc --vmss-name dcos-agent-public-D7046AAA-vmss0 --settings @custom-vmss-script-agent-public.json
